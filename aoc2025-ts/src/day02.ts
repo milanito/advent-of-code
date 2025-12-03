25a3e31cb0c13c03514b99e93a9749d2bc5f5589
m.rondeau <m.rondeau@criteo.com>
2025-12-03 08:31:00 +0100

    2025 day 2

--- aoc2025-ts/src/day02.ts
+++ aoc2025-ts/src/day02.ts
@@ -10,10 +10,72 @@
 export default function solve(input: string): string {
   const lines = input.trim().split(/\r?\n/).filter(Boolean);
   // Replace with your solution logic
-  const part1 = lines.length;
-  const part2 = lines.length;
+  const part1 = part1Solver(lines[0]);
+  const part2 = part2Solver(lines[0]);
   return `Part 1: ${part1}
 Part 2: ${part2}`;
+}
+
+const part1Solver = (line: string):number => {
+  return line.split(',')
+  .map((item) => item.split('-'))
+  .filter(items => items.length == 2)
+  .map((items) => [
+      ...Array(parseInt(items[1]) - parseInt(items[0]) + 1).keys()
+    ].map(i => i + parseInt(items[0])))
+  .flat()
+  .map(item => item.toString())
+  .reduce((acc: number, el: string): number => {
+    if (el.length % 2 != 0) {
+      return acc
+    }
+
+    let i = 0
+    let j = el.length / 2
+
+    while (j < el.length) {
+      if (el[i] != el[j]) {
+        return acc
+      }
+
+      i++
+      j++
+    }
+
+    return acc + parseInt(el)
+  }, 0)
+}
+
+const part2Solver = (line: string):number => {
+  return line.split(',')
+  .map((item) => item.split('-'))
+  .filter(items => items.length == 2)
+  .map((items) => [
+      ...Array(parseInt(items[1]) - parseInt(items[0]) + 1).keys()
+    ].map(i => i + parseInt(items[0])))
+  .flat()
+  .map(item => item.toString())
+  .reduce((acc: number, el: string): number => {
+    if (isInvalidId(el)) {
+      return acc + parseInt(el)
+    }
+
+    return acc
+  }, 0)
+}
+
+const isInvalidId = (id: string): boolean  => {
+  const n = id.length;
+
+  for (let size = 1; size <= Math.floor(n / 2); size++) {
+    if (n % size === 0) {
+      const block = id.slice(0, size);
+      if (block.repeat(n / size) === id) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 if (require.main === module) {
--- aoc2025-ts/src/day03.ts
+++ aoc2025-ts/src/day03.ts
@@ -10,11 +10,57 @@
 export default function solve(input: string): string {
   const lines = input.trim().split(/\r?\n/).filter(Boolean);
   // Replace with your solution logic
-  const part1 = lines.length;
-  const part2 = lines.length;
+  const part1 = part1Solver(lines);
+  const part2 = part2Solver(lines);
   return `Part 1: ${part1}
 Part 2: ${part2}`;
 }
+
+const part1Solver = (lines: string[]): number =>
+  lines
+  .map(line => line.split('').map(item => parseInt(item)))
+  .reduce((acc, line) => {
+    const res = getMax(line.slice(0, line.length - 1))
+    const sec = getMax(line.slice(res.index + 1))
+
+    return acc + parseInt(`${res.max}${sec.max}`)
+  }, 0)
+
+const part2Solver = (lines: string[]): number =>
+  lines
+  .map(line => line.split('').map(item => parseInt(item)))
+  .reduce((acc, line, index) => {
+    let count = 0
+    let result = []
+    while (count < line.length && result.length < 12) {
+      const res = getMax(line.slice(count, line.length - 12 + result.length + 1))
+      result.push(res.max)
+
+      if (line.length - res.index === 12 - result.length) {
+        count = line.length
+      } else {
+        count += res.index + 1
+      }
+    }
+
+
+    if (result.length < 12) {
+      result = result.concat(line.slice(line.length - 1 - result.length))
+    }
+
+    return acc + parseInt(result.join(''))
+  }, 0)
+
+const getMax = (list: number[]) => 
+  list.reduce((acc, item, index) => {
+    if (item > acc.max) {
+      return {
+        max: item, index
+      }
+    }
+
+    return acc
+  }, {max: 0, index: -1})
 
 if (require.main === module) {
   const fs = require("fs");
