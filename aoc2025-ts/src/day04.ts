97bb84b709783d928218e2d34ea88876c3d630aa
m.rondeau <m.rondeau@criteo.com>
2025-12-04 08:02:43 +0100

    2025 day 4

--- aoc2025-ts/src/day04.ts
+++ aoc2025-ts/src/day04.ts
@@ -10,10 +10,100 @@
 export default function solve(input: string): string {
   const lines = input.trim().split(/\r?\n/).filter(Boolean);
   // Replace with your solution logic
-  const part1 = lines.length;
-  const part2 = lines.length;
+  const part1 = part1Solver(lines);
+  const part2 = part2Solver(lines);
   return `Part 1: ${part1}
 Part 2: ${part2}`;
+}
+
+const prettyPrint = (grid:string[][]): void => {
+  grid.forEach(v=>console.log(...v))
+  console.log('========')
+}
+
+const constructGrid = (lines:string[]): string[][] => 
+  lines.map((line:string) => line.split(''))
+
+const isRoll = (grid: string[][], row:number, col:number) =>
+  !(row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] === '.')
+
+const checkItem = (grid:string[][], row:number, col:number) => {
+  if (grid[row][col] != '@') {
+    return false
+  }
+
+  let i = 0
+  let count = 0
+  const directions = [[0, 1], [1, 0], [1, 1], [0, -1], [-1, 0], [-1, -1], [1, -1], [-1, 1]]
+
+  while (i < directions.length && count < 4) {
+    const [drow, dcol] = directions[i] 
+    const nrow = row + drow
+    const ncol = col + dcol
+
+    if (isRoll(grid, nrow, ncol)) {
+      count += 1
+    }
+
+    ++i
+  }
+
+  return count < 4
+}
+
+const part1Solver = (lines:string[]): number => {
+  const grid = constructGrid(lines)
+  const newGrid = constructGrid(lines)
+
+  let i = 0
+  let count = 0
+
+  while (i < grid.length) {
+    let j = 0
+    while (j < grid[0].length) {
+      if (checkItem(grid, i, j)) {
+        count += 1
+        newGrid[i][j] = 'X'
+      }
+      ++j
+    }
+    ++i
+  }
+
+  return count
+}
+
+const part2Solver = (lines:string[]): number => {
+  const grid = constructGrid(lines)
+
+  let count = 0
+  let hasRemoved = []
+
+  do {
+    for (let index = 0; index < hasRemoved.length; ++index) {
+      const [row, col] = hasRemoved[index]
+      grid[row][col] = '.'
+    }
+
+    hasRemoved = []
+
+    // prettyPrint(grid)
+
+    let i = 0
+    while (i < grid.length) {
+      let j = 0
+      while (j < grid[0].length) {
+        if (checkItem(grid, i, j)) {
+          count += 1
+          hasRemoved.push([i, j])
+        }
+        ++j
+      }
+      ++i
+    }
+  } while (hasRemoved.length > 0)
+
+  return count
 }
 
 if (require.main === module) {
