0e47458e3aa414d024dce4ee9e14b053f61eefe0
Matthieu Rondeau <rondeau.matthieu.r@gmail.com>
2025-12-07 07:03:40 +0100

    2025 day 7

--- aoc2025-ts/src/day07.ts
+++ aoc2025-ts/src/day07.ts
@@ -10,10 +10,77 @@
 export default function solve(input: string): string {
   const lines = input.trim().split(/\r?\n/).filter(Boolean);
   // Replace with your solution logic
-  const part1 = lines.length;
-  const part2 = lines.length;
+  const part1 = part1Solver(lines);
+  const part2 = part2Solver(lines);
   return `Part 1: ${part1}
 Part 2: ${part2}`;
+}
+
+const part1Solver = (lines: string[]): number => {
+  const grid = lines.map(line => line.split(''))
+  let beams:string[] = []
+  let count = 0
+  let i = 0
+
+  while (i < grid[0].length && beams.length === 0) {
+    if (grid[0][i] === 'S') {
+      beams.push(`0-${i}`)
+    }
+    ++i
+  }
+
+  i = 0
+
+  while(i < grid.length - 1) {
+    const updated: string[] = []
+
+    beams.forEach(beam => {
+      const [row, col] = beam.split('-').map(it => parseInt(it))
+
+      if (grid[row + 1][col] === '.') {
+        updated.push(`${row + 1}-${col}`)
+      } else {
+        count++
+        updated.push(`${row + 1}-${col - 1}`)
+        updated.push(`${row + 1}-${col + 1}`)
+      }
+    })
+    beams = [...new Set(updated)]
+    ++i
+  }
+
+
+  return count
+}
+
+const part2Solver = (lines: string[]): number => {
+  const grid = lines.map(line => line.split(''))
+  let count = 0
+  let i = 0
+  let start: number[] = []
+
+  while (i < grid[0].length && start.length === 0) {
+    if (grid[0][i] === 'S') {
+      start = [0, i]
+    }
+    ++i
+  }
+
+  const res = new Array(grid[0].length).fill(1);
+
+  i = grid.length - 1
+
+  while (i >= 0) {
+    grid[i].forEach((it: string, idx: number): void => {
+      if (it === '^') {
+        res[idx] = res[idx - 1] + res[idx + 1]
+      }
+    })
+
+    --i
+  }
+
+  return res[start[1]]
 }
 
 if (require.main === module) {
